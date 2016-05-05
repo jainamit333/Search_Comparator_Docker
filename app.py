18f5c02f40338f11bcc74c80be1cb5e8f6c2403a
Amit Jain <Amit.Jain@makemytrip.com>
2016-05-05 17:18:56 +0530

    batch smart

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, json
-import requests
+from flask import Flask , render_template, request
+from datetime import date , datetime
 
+from helpme import file_from_url , load_file_from_upload,elements_of_same_instance,add_data_to_result_to_show,compare_dict_value,compare_list_values,compare_recommendation,compare_single_value,\
+    compare_unknown_element,compute_defference_in_attributes,convert_list_to_dic_and_filter,generate_key,scoop_json,start_compare_recommendations_dictionary
 
 app = Flask(__name__)
 
@@ -12,7 +14,7 @@ def index():
 
 @app.route('/searchcomparebatch',methods=['POST'])
 def batch_search_comparator():
-    print "start batch file"
+
     data = load_file_from_upload(request.files['batch'])
     url1 = data['host1']+'/international/json/listing/'+data['lob1']
     url2 = data['host2']+'/international/json/listing/'+data['lob2']
@@ -85,7 +87,7 @@ def batch_search_comparator():
 
 @app.route('/searchcompare',methods=['POST'])
 def search_comparator():
-    print("Start Search Comparator")
+
     result_to_show = []
     recommandation_wise_result = {}
     attribute_difference_recommendation_wise_level1 = {}
@@ -140,178 +142,113 @@ def search_comparator():
 
 
 
-# noinspection PyBroadException
-def compute_defference_in_attributes(old_data, new_data, difference_in_reccomendation_list):
-    difference_in_reccomendation_list.append(set(old_data.keys()) - set(new_data.keys()))
-    difference_in_reccomendation_list.append(set(new_data.keys()) - set(old_data.keys()))
+@app.route('/searchcomparebatchsmart',methods=['POST'])
+def batch_search_comparator_lage_raho():
+    data = load_file_from_upload(request.files['batch'])
+    url1 = data['host1']+'/international/json/listing/'+data['lob1']
+    url2 = data['host2']+'/international/json/listing/'+data['lob2']
+    arrayoftriptype = ['OW','RT']
+
+    case_wise_result_to_show = {}
+    case_wise_recommandation_wise_result = {}
+    case_wise_attribute_difference_recommendation_wise = {}
+    attribute_difference_recommendation_nested = {}
+    month_array = ['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP',"OCT",'NOV','DEC']
+    for case in data['cases']:
+        fromcity = case['from']
+        tocity = case['to']
+        for rtcase in arrayoftriptype:
+            today = date.today()
+            current_month = today.month+2
+            current_year = today.year
+            if current_month > 12:
+                current_month = 2
+                current_year = current_year + 1
+
+            start_date = ["1"+month_array[current_month-1]+str(current_year),"2"+month_array[current_month-1]+str(current_year),
+                          "3"+month_array[current_month-1]+str(current_year),"4"+month_array[current_month-1]+str(current_year),
+                          "5"+month_array[current_month-1]+str(current_year),"6"+month_array[current_month-1]+str(current_year),"7"+month_array[current_month-1]+str(current_year)]
+            end_date = ["11"+month_array[current_month-1]+str(current_year),"12"+month_array[current_month-1]+str(current_year),
+                        "13"+month_array[current_month-1]+str(current_year),"14"+month_array[current_month-1]+str(current_year),
+                        "15"+month_array[current_month-1]+str(current_year),"16"+month_array[current_month-1]+str(current_year),"17"+month_array[current_month-1]+ str(current_year)]
+            datecounter = 0
+            for datecase in start_date:
+                paxtypearray = ['A-1','A-2','A-1-C-1','A-2-C-1','A-1-C-1-I-1','A-2-C-1-I-1','A-2-C-2-I-1']
+                q1 = fromcity+"-"+tocity+"-D-"+datecase
+                if(rtcase == 'RT'):
+                    q1 = q1 +"_"+tocity+"-"+fromcity+"-D-"+end_date[datecounter]
+                    datecounter = datecounter+1
+
+                for paxcase in paxtypearray:
+                    cabinarray = ['E','B','F','PE']
+                    for cabincase in cabinarray:
+                        case_of_fire = "/"+rtcase+"/"+q1+"/"+paxcase+"/"+cabincase
+                        first_url_final = url1+case_of_fire
+                        second_url_final = url2+case_of_fire
+
+                        result_to_show = []
+                        recommandation_wise_result = {}
+                        attribute_difference_recommendation_wise = {}
+                        attribute_difference_recommendation_wise_nested = {}
+                        case_wise_result_to_show[case_of_fire] = result_to_show
+                        case_wise_recommandation_wise_result[case_of_fire]= recommandation_wise_result
+                        case_wise_attribute_difference_recommendation_wise[case_of_fire] = attribute_difference_recommendation_wise
+                        attribute_difference_recommendation_nested[case_of_fire] = attribute_difference_recommendation_wise_nested
+
+                        # load Files From Url
+                        old_json = file_from_url(first_url_final)
+                        new_json = file_from_url(second_url_final)
+                        if old_json == "Connection Error":
+                            add_data_to_result_to_show(result_to_show, "Error while uploading File 1")
+                            continue
+                            #return render_template('result.html', data=result_to_show,data2 = recommandation_wise_result,data3 = attribute_difference_recommendation_wise)
+                        if new_json == "Connection Error":
+                            add_data_to_result_to_show(result_to_show, "Error while uploading File 2")
+                            continue
+                            #return render_template('result.html', data=result_to_show,data2 = recommandation_wise_result,data3 = attribute_difference_recommendation_wise)
+
+                        old_json = scoop_json(old_json, 0, "results")
+                        new_json = scoop_json(new_json, 0, "results")
+
+                        add_data_to_result_to_show(result_to_show, 'Length of results in File 1 without filter ' + str(len(old_json)))
+                        add_data_to_result_to_show(result_to_show, 'Length of results in File 2 without filter ' + str(len(new_json)))
+
+                        # create dictionary and filter according to airline
+                        services = request.form['service']
+                        old_dictionary = convert_list_to_dic_and_filter(old_json, request.form['dictionary_id_1'],
+                                                        "REM_AFTER_LAST_UNDERSCORE", services)
+                        new_dictionary = convert_list_to_dic_and_filter(new_json, request.form['dictionary_id_2'],
+                                                        "REM_AFTER_LAST_UNDERSCORE", services)
+
+                        old_json = None
+                        new_json = None
+
+                        add_data_to_result_to_show(result_to_show, 'Length of results in File 1 after filter ' + str(len(old_dictionary)))
+                        add_data_to_result_to_show(result_to_show, 'Length of results in File 2 after filter ' + str(len(new_dictionary)))
 
 
+                        if not isinstance(old_dictionary, dict):
+                            add_data_to_result_to_show(result_to_show, "Unable to convert dictionary for file 1 , Internal Error")
+                            continue
+                            #return render_template("result.html", data=result_to_show,data2 = recommandation_wise_result,data3=attribute_difference_recommendation_wise)
+                        if not isinstance(new_dictionary, dict):
+                            add_data_to_result_to_show(result_to_show, "Unable to convert dictionary for file 2 , Internal Error")
+                            continue
+                            #return render_template("result.html", data=result_to_show,data2 = recommandation_wise_result,data3=attribute_difference_recommendation_wise)
 
-def start_compare_recommendations_dictionary(old_dictionary, new_dictionary, result_to_show, recommandation_wise_result,
-                                             attribute_difference_recommendation_wise,attribute_difference_recommendation_nested):
-    # loop the dictionary
-    for key_attr in old_dictionary:
-        recommendation_list = []
-        difference_in_reccomendation_list = []
-        difference_in_reccomendation_list_nested = []
-        recommandation_wise_result[key_attr] = recommendation_list
-        attribute_difference_recommendation_wise[key_attr] = difference_in_reccomendation_list
-        attribute_difference_recommendation_nested[key_attr] = difference_in_reccomendation_list_nested
-        try:
-            new_data = new_dictionary[key_attr]
-        except Exception:
-            add_data_to_result_to_show(recommendation_list, "Reccomendation not found in File 2 for key" + key_attr)
-            continue
-        old_data = old_dictionary[key_attr]
-        compute_defference_in_attributes(old_data,new_data,difference_in_reccomendation_list)
-        compare_recommendation(old_data, new_data, recommendation_list, key_attr,difference_in_reccomendation_list_nested)
+                        start_compare_recommendations_dictionary(old_dictionary, new_dictionary, result_to_show, recommandation_wise_result,attribute_difference_recommendation_wise,attribute_difference_recommendation_wise_nested)
 
 
-def compare_recommendation(recommendation_old, recommendation_new, result_to_show, ket_attr,difference_in_reccomendation_list_nested):
-
-    if elements_of_same_instance(recommendation_old, recommendation_new):
-
-        if isinstance(recommendation_new, dict):
-            compare_dict_value(recommendation_old,recommendation_new,result_to_show,ket_attr,"",difference_in_reccomendation_list_nested)
-        elif isinstance(recommendation_new, (list, set)):
-
-            compare_list_values(recommendation_old, recommendation_new, result_to_show, ket_attr, ""),difference_in_reccomendation_list_nested
-        elif isinstance(recommendation_new, (str, int, float, bool,unicode)):
-
-            compare_single_value(recommendation_old, recommendation_new, result_to_show, ket_attr, ket_attr,difference_in_reccomendation_list_nested)
-
-    else:
-        add_data_to_result_to_show(result_to_show, "Recommendation is not of same type insatance for key " + ket_attr)
-
-
-def compare_single_value(recommendation_old, recommendation_new, result_to_show, ket_attr, current_attribute_name,difference_in_reccomendation_list_nested):
-
-    if not recommendation_old == recommendation_new:
-        print "Value does not match"
-        add_data_to_result_to_show(result_to_show,
-                                   "Value does not match in Recommendation " + str(ket_attr) + " Attribute Name " +
-                                   current_attribute_name+" with values :---- "+str(recommendation_old)+" -----  "+str(recommendation_new))
-
-
-def compare_unknown_element(old_data, new_data, result_to_show, ket_attr, current_attribute_name,difference_in_reccomendation_list_nested):
-    if elements_of_same_instance(old_data, new_data):
-        print "start comparing"
-
-        if isinstance(old_data, dict):
-            print "start dic comparision"
-            compare_dict_value(old_data,new_data,result_to_show,ket_attr,current_attribute_name,difference_in_reccomendation_list_nested)
-        elif isinstance(old_data, (list, set)):
-            print "comapre list or set"
-            compare_list_values(old_data, new_data, result_to_show, ket_attr, current_attribute_name,difference_in_reccomendation_list_nested)
-        elif isinstance(old_data, (str, int, float, bool,unicode)):
-            print "Compare single value"
-            compare_single_value(old_data, new_data, result_to_show, ket_attr, current_attribute_name,difference_in_reccomendation_list_nested)
-    else:
-        add_data_to_result_to_show(result_to_show, "Recommendation is not of same type insatance for key " + ket_attr)
-
-
-def compare_list_values(list_one, list_two, result_to_show, ket_attr, current_attribute_name,difference_in_reccomendation_list_nested):
-    print "start comparing list"
-    if not len(list_one) == len(list_two):
-        add_data_to_result_to_show(result_to_show,
-                                   "Length of List found is not equal for Recommendation " + ket_attr +
-                                   " Attribute Name " + current_attribute_name+" value 1 ----"+str(list_one)+"   ----value 2 -------"+str(list_two))
-        add_data_to_result_to_show(difference_in_reccomendation_list_nested,"Length of List found is not equal for Recommendation " + ket_attr +
-                                   " Attribute Name " + current_attribute_name+" value 1 ----"+str(list_one)+"   ----value 2 -------"+str(list_two))
-    else:
-        counter = 0
-        for old_data in list_one:
-            compare_unknown_element(old_data, list_two[counter], result_to_show, ket_attr,
-                                    current_attribute_name + ":" + str(counter),difference_in_reccomendation_list_nested)
-            counter=counter+1
-
-
-def compare_dict_value(dict_old, dict_new, result_to_show, ket_attr, current_attribute_name,difference_in_reccomendation_list_nested):
-    print "Comparing dictionary value"
-    for attribute in dict_old:
-        try:
-            new_value = dict_new[attribute]
-        except:
-            add_data_to_result_to_show(result_to_show, "Element not found for Recommendation with key " + ket_attr
-                                       + " attribute " + current_attribute_name + attribute)
-            add_data_to_result_to_show(difference_in_reccomendation_list_nested, "Element not found for Recommendation with key " + ket_attr
-                                       + " attribute " + current_attribute_name + attribute)
-        old_value = dict_old[attribute]
-        compare_unknown_element(old_value, new_value, result_to_show, ket_attr,current_attribute_name + "." + attribute,difference_in_reccomendation_list_nested)
-
-
-
-def file_from_url(url):
-
-    try:
-        u_response = requests.get(url)
-    except requests.ConnectionError:
-        return "Connection Error"
-    data = json.loads(u_response.text)
-    return data
-
-def load_file_from_upload(file):
-    try:
-        data = file.read()
-    except:
-        return "Connection Error"
-    data = json.loads(data)
-    return data
+    return render_template('result.html', data=case_wise_result_to_show,data2 = case_wise_recommandation_wise_result,
+                           data3 = case_wise_attribute_difference_recommendation_wise,data4 = attribute_difference_recommendation_nested)
 
 
 
 
-def add_data_to_result_to_show(result_to_show, param):
-    print "adding value to result to show"
-    result_to_show.insert(len(result_to_show), str(param))
 
 
-def scoop_json(data, index_of_data, attrs):
-
-    if isinstance(data,dict):
-        attrs = attrs.split(".")
-        for att in attrs:
-            data = data[att]
-        return data
-    else:
-        if len(attrs) > 0:
-            attrs = attrs.split(".")
-            data = data[int(index_of_data)]
-            for att in attrs:
-                data = data[att]
-            return data
-        else:
-            return data
 
 
-def convert_list_to_dic_and_filter(data, key_attr, appender, service):
-
-    dictionary = {}
-    for value in data:
-        if len(service) > 0:
-            if (value[key_attr].lower()).find(service.lower()) != -1:
-                key = generate_key(value[key_attr], appender)
-                dictionary[key] = value
-        else:
-            key = generate_key(value[key_attr], appender)
-            dictionary[key] = value
-
-    return dictionary
-
-
-def generate_key(value, appender):
-    if appender == 'REM_AFTER_LAST_UNDERSCORE':
-        value = value[0:value.rindex("_")]
-        return value.lower()
-    if appender == 'NOTHING':
-        return value.lower()
-
-
-def elements_of_same_instance(element_1, element_2):
-    if type(element_1) == type(element_2):
-        return True
-    else:
-        return False
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
---
+++ helpme.py
@@ -0,0 +1,183 @@
+from flask import Flask, render_template, request, json
+import requests
+
+
+
+
+
+# noinspection PyBroadException
+def compute_defference_in_attributes(old_data, new_data, difference_in_reccomendation_list):
+    difference_in_reccomendation_list.append(set(old_data.keys()) - set(new_data.keys()))
+    difference_in_reccomendation_list.append(set(new_data.keys()) - set(old_data.keys()))
+
+
+
+def start_compare_recommendations_dictionary(old_dictionary, new_dictionary, result_to_show, recommandation_wise_result,
+                                             attribute_difference_recommendation_wise,attribute_difference_recommendation_nested):
+    # loop the dictionary
+    for key_attr in old_dictionary:
+        recommendation_list = []
+        difference_in_reccomendation_list = []
+        difference_in_reccomendation_list_nested = []
+        recommandation_wise_result[key_attr] = recommendation_list
+        attribute_difference_recommendation_wise[key_attr] = difference_in_reccomendation_list
+        attribute_difference_recommendation_nested[key_attr] = difference_in_reccomendation_list_nested
+        try:
+            new_data = new_dictionary[key_attr]
+        except Exception:
+            add_data_to_result_to_show(recommendation_list, "Reccomendation not found in File 2 for key" + key_attr)
+            continue
+        old_data = old_dictionary[key_attr]
+        compute_defference_in_attributes(old_data,new_data,difference_in_reccomendation_list)
+        compare_recommendation(old_data, new_data, recommendation_list, key_attr,difference_in_reccomendation_list_nested)
+
+
+def compare_recommendation(recommendation_old, recommendation_new, result_to_show, ket_attr,difference_in_reccomendation_list_nested):
+
+    if elements_of_same_instance(recommendation_old, recommendation_new):
+
+        if isinstance(recommendation_new, dict):
+            compare_dict_value(recommendation_old,recommendation_new,result_to_show,ket_attr,"",difference_in_reccomendation_list_nested)
+        elif isinstance(recommendation_new, (list, set)):
+
+            compare_list_values(recommendation_old, recommendation_new, result_to_show, ket_attr, ""),difference_in_reccomendation_list_nested
+        elif isinstance(recommendation_new, (str, int, float, bool,unicode)):
+
+            compare_single_value(recommendation_old, recommendation_new, result_to_show, ket_attr, ket_attr,difference_in_reccomendation_list_nested)
+
+    else:
+        add_data_to_result_to_show(result_to_show, "Recommendation is not of same type insatance for key " + ket_attr)
+
+
+def compare_single_value(recommendation_old, recommendation_new, result_to_show, ket_attr, current_attribute_name,difference_in_reccomendation_list_nested):
+
+    if not recommendation_old == recommendation_new:
+        print "Value does not match"
+        add_data_to_result_to_show(result_to_show,
+                                   "Value does not match in Recommendation " + str(ket_attr) + " Attribute Name " +
+                                   current_attribute_name+" with values :---- "+str(recommendation_old)+" -----  "+str(recommendation_new))
+
+
+def compare_unknown_element(old_data, new_data, result_to_show, ket_attr, current_attribute_name,difference_in_reccomendation_list_nested):
+    if elements_of_same_instance(old_data, new_data):
+        print "start comparing"
+
+        if isinstance(old_data, dict):
+            print "start dic comparision"
+            compare_dict_value(old_data,new_data,result_to_show,ket_attr,current_attribute_name,difference_in_reccomendation_list_nested)
+        elif isinstance(old_data, (list, set)):
+            print "comapre list or set"
+            compare_list_values(old_data, new_data, result_to_show, ket_attr, current_attribute_name,difference_in_reccomendation_list_nested)
+        elif isinstance(old_data, (str, int, float, bool,unicode)):
+            print "Compare single value"
+            compare_single_value(old_data, new_data, result_to_show, ket_attr, current_attribute_name,difference_in_reccomendation_list_nested)
+    else:
+        add_data_to_result_to_show(result_to_show, "Recommendation is not of same type insatance for key " + ket_attr)
+
+
+def compare_list_values(list_one, list_two, result_to_show, ket_attr, current_attribute_name,difference_in_reccomendation_list_nested):
+    print "start comparing list"
+    if not len(list_one) == len(list_two):
+        add_data_to_result_to_show(result_to_show,
+                                   "Length of List found is not equal for Recommendation " + ket_attr +
+                                   " Attribute Name " + current_attribute_name+" value 1 ----"+str(list_one)+"   ----value 2 -------"+str(list_two))
+        add_data_to_result_to_show(difference_in_reccomendation_list_nested,"Length of List found is not equal for Recommendation " + ket_attr +
+                                   " Attribute Name " + current_attribute_name+" value 1 ----"+str(list_one)+"   ----value 2 -------"+str(list_two))
+    else:
+        counter = 0
+        for old_data in list_one:
+            compare_unknown_element(old_data, list_two[counter], result_to_show, ket_attr,
+                                    current_attribute_name + ":" + str(counter),difference_in_reccomendation_list_nested)
+            counter=counter+1
+
+
+def compare_dict_value(dict_old, dict_new, result_to_show, ket_attr, current_attribute_name,difference_in_reccomendation_list_nested):
+    print "Comparing dictionary value"
+    for attribute in dict_old:
+        try:
+            new_value = dict_new[attribute]
+        except:
+            add_data_to_result_to_show(result_to_show, "Element not found for Recommendation with key " + ket_attr
+                                       + " attribute " + current_attribute_name + attribute)
+            add_data_to_result_to_show(difference_in_reccomendation_list_nested, "Element not found for Recommendation with key " + ket_attr
+                                       + " attribute " + current_attribute_name + attribute)
+        old_value = dict_old[attribute]
+        compare_unknown_element(old_value, new_value, result_to_show, ket_attr,current_attribute_name + "." + attribute,difference_in_reccomendation_list_nested)
+
+
+
+def file_from_url(url):
+
+    try:
+        u_response = requests.get(url)
+    except requests.ConnectionError:
+        return "Connection Error"
+    data = json.loads(u_response.text)
+    return data
+
+def load_file_from_upload(file):
+    try:
+        data = file.read()
+    except:
+        return "Connection Error"
+    data = json.loads(data)
+    return data
+
+
+
+
+def add_data_to_result_to_show(result_to_show, param):
+    print "adding value to result to show"
+    result_to_show.insert(len(result_to_show), str(param))
+
+
+def scoop_json(data, index_of_data, attrs):
+
+    if isinstance(data,dict):
+        attrs = attrs.split(".")
+        for att in attrs:
+            data = data[att]
+        return data
+    else:
+        if len(attrs) > 0:
+            attrs = attrs.split(".")
+            data = data[int(index_of_data)]
+            for att in attrs:
+                data = data[att]
+            return data
+        else:
+            return data
+
+
+def convert_list_to_dic_and_filter(data, key_attr, appender, service):
+
+    dictionary = {}
+    for value in data:
+        if len(service) > 0:
+            if (value[key_attr].lower()).find(service.lower()) != -1:
+                key = generate_key(value[key_attr], appender)
+                dictionary[key] = value
+        else:
+            key = generate_key(value[key_attr], appender)
+            dictionary[key] = value
+
+    return dictionary
+
+
+def generate_key(value, appender):
+    if appender == 'REM_AFTER_LAST_UNDERSCORE':
+        value = value[0:value.rindex("_")]
+        return value.lower()
+    if appender == 'NOTHING':
+        return value.lower()
+
+
+def elements_of_same_instance(element_1, element_2):
+    if type(element_1) == type(element_2):
+        return True
+    else:
+        return False
+
+
+
+
